ad85bdcfc6c85c9fffaf10d82281a154e770c2ca
Didar.Bhullar <didar.s.bhullar@gmail.com>
2017-10-30 17:00:16 -0700

    US26: Load data into our website

--- hotels/models.py
+++ hotels/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+import csv
 
 
 class Hotel(models.Model):
     name = models.CharField(max_length=300)
-    address = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    city = models.CharField(max_length=300, default= "NO CITY FOUND")
+    address = models.CharField(max_length=300)
+    phone = models.CharField(max_length=12, default = "NO PHONE FOUND")
+    rating = models.DecimalField(max_digits = 6, decimal_places=2, default = 0.00)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default= 0.00)
     # expect a string with a delimiter of ','
     images = models.CharField(max_length=5000, null=True)
 
@@ -12,6 +16,5 @@ class Hotel(models.Model):
         return self.name
 
     def images_as_list(self):
-        return self.images.split(',')
-
+        return self.images.split(' ')
 
