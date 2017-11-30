c14a5edd73e65a0c36d580bc893b335f54fc9c08
hammaadilyas <sjilyas408@yahoo.com>
2017-11-29 18:05:28 -0800

    fixes baby

--- resources/import.py
+++ resources/import.py
@@ -1,6 +1,9 @@
 import sys, os, django
 
-sys.path.append("E:/CMPE165/crunchy/crunchy") #here store is root folder(means parent).
+
+sys.path.append("C:/Users/MADDI/Desktop/crunchy/crunchy") #here store is root folder(means parent).
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crunchy.settings")
 django.setup()
 
